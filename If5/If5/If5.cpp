4e3ed087c8b64f85dd201b22bba08667be57a940
Radeon590 <lapochtaleos@gmail.com>
2020-01-19 14:25:57 +0300

    Update If5.cpp

--- If5/If5/If5.cpp
+++ If5/If5/If5.cpp
@@ -1,11 +1,24 @@
 ﻿#include <iostream>
 
-
+using namespace std;
 
 //Даны три целых числа. Найти количество положительных и количество от-
 //rицательных чисел в исходном наборе.
+
 int main()
 {
-    
+    int a, p = 0, o = 0;
+    for (int i = 0; i <= 2; i++) {
+        cout << "Enter" << endl;
+        cin >> a;
+        if (a > 0) {
+            p++;
+        } else if (a < 0) {
+            o++;
+        }
+    }
+    cout << "P =\t" << p << endl;
+    cout << "O =\t" << o << endl;
+    return 0;
 }
 
