ac800936680a9c11dde837c0bd5ad0990d835d48
Radeon590 <lapochtaleos@gmail.com>
2020-01-20 19:15:17 +0300

    If15done

---
+++ If15/If15/Program.cs
@@ -0,0 +1,26 @@
+﻿//Даны три числа.Найти сумму двух наибольших из них.
+using System;
+
+namespace If15
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double A = 0, B = 0, C = 0;
+            double a = Convert.ToDouble(Console.ReadLine());
+            double b = Convert.ToDouble(Console.ReadLine());
+            double c = Convert.ToDouble(Console.ReadLine());
+            if (a > b || a > c) {
+                A = a;
+            }
+            if (b > a || b > c) {
+                B = b;
+            }
+            if (c > a || c > b) {
+                C = c;
+            }
+            Console.WriteLine(A + B + C);
+        }
+    }
+}
