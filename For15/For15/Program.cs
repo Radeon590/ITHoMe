5b96b2e29714654d0fb79ede4230ea168e81098e
Radeon590 <lapochtaleos@gmail.com>
2020-01-21 00:08:05 +0300

    For15done

---
+++ For15/For15/Program.cs
@@ -0,0 +1,23 @@
+﻿//Дано вещественное число A и целое число N (> 0). Найти A в степе-
+//ни N :
+//AN = A·A· . . . ·A
+//(числа A перемножаются N раз).
+
+using System;
+
+namespace For15
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int A = Convert.ToInt32(Console.ReadLine());
+            int N = Convert.ToInt32(Console.ReadLine());
+            int a = A;
+            for (int i = 2; i <= N; i++) {
+                a = a * A;
+            }
+            Console.WriteLine(a);
+        }
+    }
+}
