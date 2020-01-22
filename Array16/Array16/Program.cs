89911f826ebdec22b82bf1b8c0a6d867c6b45c98
Radeon590 <lapochtaleos@gmail.com>
2020-01-22 16:27:10 +0300

    Array16 done

---
+++ Array16/Array16/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Array16
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int N = Convert.ToInt32(Console.ReadLine());
+            int[] nums = new int[N];
+            for (int i = 0; i < N; i++)
+            {
+                nums[i] = Convert.ToInt32(Console.ReadLine());
+            }
+            Console.WriteLine("Ok");
+            for (int i = 0; i < N; i++)
+            {
+                Console.WriteLine(nums[i]);
+                Console.WriteLine(nums[N - 1 - i]);
+            }
+        }
+    }
+}
