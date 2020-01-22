a4cfb113dab623b7cbd396b3c648af3504498c29
Radeon590 <lapochtaleos@gmail.com>
2020-01-22 16:21:41 +0300

    Array17 done

---
+++ Array17/Array17/Program.cs
@@ -0,0 +1,32 @@
+﻿//Дан массив A размера N.Вывести его элементы в следующем порядке:
+//A0, A1, AN , AN−1, A2, A3, AN−2, AN−3, . . . .
+using System;
+
+namespace Array17
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 0, b = 0;
+            int N = Convert.ToInt32(Console.ReadLine());
+            int[] nums = new int[N];
+            for(int i = 0; i < N; i++){
+                nums[i] = Convert.ToInt32(Console.ReadLine());
+            }
+            Console.WriteLine("Ok");
+            for (int i = 0; i < N; i++) {
+                Console.WriteLine(nums[i]);
+                b++;
+                if (b == 2) {
+                    Console.WriteLine(nums[N - 1 - a]);
+                    a++;
+                    Console.WriteLine(nums[N - 1 - a]);
+                    a++;
+                    b = 0;
+                }
+            }
+            
+        }
+    }
+}
