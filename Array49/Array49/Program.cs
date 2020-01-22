fc8989aaf943cca2a0224cd92b938d3e11de7426
Radeon590 <lapochtaleos@gmail.com>
2020-01-22 16:31:22 +0300

    Create Array49

---
+++ Array49/Array49/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Array49
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int N = Convert.ToInt32(Console.ReadLine());
+            int[] nums = new int[N];
+            for (int i = 0; i < N; i++)
+            {
+                nums[i] = Convert.ToInt32(Console.ReadLine());
+            }
+            Console.WriteLine("Ok");
+            for (int i = 0; i < N; i++) {
+                if ((i + 1) == ) { 
+                
+                }
+            }
+        }
+    }
+}
