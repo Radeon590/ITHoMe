8cd2a925f3644ead0764acc0c2a1e4ce141535d7
Radeon590 <lapochtaleos@gmail.com>
2020-01-25 21:34:58 +0300

    Array88 done

---
+++ Array88/Array88/Program.cs
@@ -0,0 +1,24 @@
+﻿//Дан массив размера N, все элементы которого, кроме последнего,
+//упорядочены по возрастанию.Сделать массив упорядоченным, переме-
+//стив последний элемент на новую позицию.
+using System;
+
+namespace Array88
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int N = Convert.ToInt32(Console.ReadLine());
+            int[] A = new int[N];
+            for (int i = 0; i < N; i++) { 
+            A[i] = Convert.ToInt32(Console.ReadLine());
+            }
+            Array.Sort(A);
+            for (int i = 0; i < N; i++)
+            {
+                Console.WriteLine(A[i]);
+            }
+        }
+    }
+}
