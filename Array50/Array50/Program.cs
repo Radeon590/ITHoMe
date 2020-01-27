9f71ba436e388a6416115a8871637cf70779ff34
HappyFox002 <45999287+HappyFox002@users.noreply.github.com>
2020-01-27 15:05:19 +0300

    Правильное решение

--- Array50/Array50/Program.cs
+++ Array50/Array50/Program.cs
@@ -10,21 +10,25 @@ namespace Array50
     {
         static void Main(string[] args)
         {
-            //
+            //Формирование массива
             int N = Convert.ToInt32(Console.ReadLine());
             int[] A = new int[N];
-            for (int i = 0; i < N; i++) {
+            for (int i = 0; i < N; i++)
+            {
                 A[i] = Convert.ToInt32(Console.ReadLine());
             }
-            //
-
-            for (int a = 0; a < N; a++) {
-                for (int b = 0; b < N; b++) {
-                    if (A[a]) { 
-                    
-                    }
+            //Подсчёт
+            int counter = 0;
+            for (int i = 0; i < N-1; i++) {
+                if (A[i] > A[i + 1]) {
+                    counter++;
                 }
             }
+            //Дополнительная проверка
+            if (A[N - 1] > A[0])
+                counter++;
+
+            Console.WriteLine("Кол-во инверсий: " + counter);
         }
     }
 }
